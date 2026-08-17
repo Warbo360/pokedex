236bc91625aa00bb5772a620bca2e09f47cd31a0
Walter Betz Jr <129965715+Warbo360@users.noreply.github.com>
2026-08-16 20:40:34 -0500

    Help command implementation

--- repl.go
+++ repl.go
@@ -7,17 +7,56 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name string
+	description string
+	callback func() error
+}
+
+var cliCommands map[string]cliCommand
+
+func init() {
+
+	/* Map of available CLI commands for the program */
+
+	cliCommands = map[string]cliCommand{
+		"exit": {
+			name: "exit",
+			description: "Exits Pokedex REPL",
+			callback: exit,
+		},
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: help,
+		},
+	}
+}
+
 func startRepl() {
+
+	/* Takes in input from the user and cleans it */
+
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
-		input := scanner.Text()
-		cleanedInput := cleanInput(input)
-		if len(cleanedInput) == 0 {
+		input := cleanInput(scanner.Text())
+		if len(input) == 0 {
 			continue
 		}
-		fmt.Printf("Your command was: %v\n", cleanedInput[0])
+		command := input[0]
+
+	/* Matches command input to callback for that command from cliCommands */
+
+		switch command {
+		case "exit":
+			cliCommands["exit"].callback()
+		case "help":
+			cliCommands["help"].callback()
+		default:
+			fmt.Println("Unknown Command")
+		}
 	}
 }
 
@@ -27,4 +66,19 @@ func cleanInput(text string) []string {
 	splitText := strings.Fields(loweredText)
 	return splitText
 
+}
+
+func exit() error {
+	fmt.Print("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func help() error {
+	fmt.Printf("Welcome to Pokedex!\n")
+	fmt.Printf("Usage:\n")
+	for name, cmd := range cliCommands {
+		fmt.Printf("  - %v: %v\n", name, cmd.description)
+	}
+	return nil
 }
