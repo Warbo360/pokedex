0476e4ca535af04f98bfc1d4810cb87d74e90d92
Walter Betz Jr <129965715+Warbo360@users.noreply.github.com>
2026-09-03 20:27:30 -0500

    API handling for getting a location-area from pokeapi

--- internal/pokeapi/location_get.go
+++ internal/pokeapi/location_get.go
@@ -1,4 +1,4 @@
-package api
+package pokeapi
 
 import (
 	"encoding/json"
@@ -8,12 +8,54 @@ import (
 
 func (c *Client) GetLocationArea(area string) (RespLocationArea, error) {
 	url := baseURL + "/location-area/" + area
+
+	// Check cache first for url and retrieve data if present
+
 	if value, ok := c.cache.Get(url); ok {
 		locationAreaResp := RespLocationArea{}
 		err := json.Unmarshal(value, &locationAreaResp)
 		if err != nil {
-			return RespLocationArea, err
+			return locationAreaResp, err
 		}
 		return locationAreaResp, nil
 	}
+
+	// If cache misses, build http GET request message and error check
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return RespLocationArea{}, err
+	}
+
+	// Send request over the network and error check
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return RespLocationArea{}, err
+	}
+
+	// defer body closing from readall io of the response body
+
+	defer resp.Body.Close()
+	dat, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return RespLocationArea{}, err
+	}
+
+	// Make an empty RespLocationArea struct and unmarshal the json into that struct
+
+	locationAreaResp := RespLocationArea{}
+	err = json.Unmarshal(dat, &locationAreaResp)
+	if err != nil {
+		return RespLocationArea{}, err
+	}
+
+	// Store the raw data into the cache under the built url
+
+	c.cache.Add(url, dat)
+
+	// return built struct
+
+	return locationAreaResp, nil
+
 }
